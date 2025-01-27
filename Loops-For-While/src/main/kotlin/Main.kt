b9766f5e7b07c31461504c56ac64450ce8bf85f0
DanZx819 <147755543+DanZx819@users.noreply.github.com>
2025-01-27 17:55:30 -0300

    Loops for/while

---
+++ Loops-For-While/src/main/kotlin/Main.kt
@@ -0,0 +1,23 @@
+package org.example
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+fun main() {
+//  for (i in 1 ..  10){
+//      if(i == 4){
+//          break
+//      }
+//      println(i)
+//  }
+    var x: Int = 0
+    while(true){
+        if(x < 10){
+            x++
+            println(x)
+        }else{
+            break
+        }
+
+    }
+
+}
