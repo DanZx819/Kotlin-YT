2ad4453a9e4ec71b64f59290da307fc74474ee3a
DanZx819 <147755543+DanZx819@users.noreply.github.com>
2025-01-28 15:32:25 -0300

    Simulando login

--- Loops-For-While/src/main/kotlin/Main.kt
+++ Loops-For-While/src/main/kotlin/Main.kt
@@ -9,13 +9,32 @@ fun main() {
 //      }
 //      println(i)
 //  }
-    var x: Int = 0
-    while(true){
-        if(x < 10){
-            x++
-            println(x)
-        }else{
+//    var x: Int = 0
+//    while(true){
+//        if(x < 10){
+//            x++
+//            println(x)
+//        }else{
+//            break
+//        }
+//
+//    }
+    
+    var tentativas = 0
+    var senha = "123"
+
+    while (tentativas < 3){
+        println("Digite a sua senha: ")
+        var senhadig = readln() ?: ""
+        if(senhadig.equals(senha)){
+            println("Acesso liberado!")
             break
+        }else{
+            println("Senha ou usuario inválido.")
+            tentativas++
+        }
+        if(tentativas == 3){
+            println("Usuario bloqueado, procure a sua agência.")
         }
 
     }
